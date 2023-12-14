91ae7f9296da0456a0b7a25c57ae3150027dcb51
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2023-12-14 09:58:38 +0000

    Added encoding to open() in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-with open("requirements.txt") as f:
+with open("requirements.txt", encoding="utf-8") as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if "git+" not in x]
 
