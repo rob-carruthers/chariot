6d2976feb5bffe6ef9aa71bec3bb747be7595eab
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2023-12-15 12:46:17 +0000

    Added base FastAPI interface

---
+++ chariot/api.py
@@ -0,0 +1,36 @@
+"""FastAPI endpoints for Chariot"""
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .interface import ChariotJourneyPipeline
+
+app = FastAPI()
+app.state.pipeline = ChariotJourneyPipeline()
+
+origins = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000"
+    # Add other origins if necessary
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # List of allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
+
+@app.get("/ping")
+def ping():
+    """
+    Return a ping if the api is able to respond.
+    """
+    return {"response": "ping"}
+
+@app.get("/")
+async def chariot_request(query):
+    """
+    Process a natural language TFL journey request.
+    """
+    result = app.state.pipeline.request_from_natural_language(query)
+    return result
